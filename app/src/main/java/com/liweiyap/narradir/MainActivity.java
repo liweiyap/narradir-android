8bc2d151606b9734cf8c6341205cf6cec1f815e4
liweiyap <liweiyap@gmail.com>
2021-02-18 20:18:01 +0000

    Prevent app restart on phone rotation. Fix screen orientation to portrait anyway.

--- app/src/main/java/com/liweiyap/narradir/MainActivity.java
+++ app/src/main/java/com/liweiyap/narradir/MainActivity.java
@@ -1,5 +1,6 @@
 package com.liweiyap.narradir;
 
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.testwavenet);
         mp.start();
