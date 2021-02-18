be27cb8d0f82b4c97055d6e92732d0e567f20734
liweiyap <liweiyap@gmail.com>
2021-02-18 21:00:04 +0000

    Also hide navigation bar when temporarily not required.

--- app/src/main/java/com/liweiyap/narradir/MainActivity.java
+++ app/src/main/java/com/liweiyap/narradir/MainActivity.java
@@ -3,6 +3,7 @@ package com.liweiyap.narradir;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);  // make the below show-/hide-changes temporary
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |  // hide navigation bar
+            View.SYSTEM_UI_FLAG_FULLSCREEN |  // hide status bar
+            View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
