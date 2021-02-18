2be1becabf31219845b3c63f0dc36c996a25cfde
liweiyap <liweiyap@gmail.com>
2021-02-18 20:45:04 +0000

    Permanently hide action bar. Make status bar translucent and hide it when temporarily not required.

--- app/src/main/java/com/liweiyap/narradir/MainActivity.java
+++ app/src/main/java/com/liweiyap/narradir/MainActivity.java
@@ -3,6 +3,7 @@ package com.liweiyap.narradir;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
