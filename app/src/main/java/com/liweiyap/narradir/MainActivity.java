0ac3f5c32f6e567a4b8db96baf1179332b77f64d
liweiyap <liweiyap@gmail.com>
2021-03-03 19:30:49 +0000

    Add Merlin drawable

--- app/src/main/java/com/liweiyap/narradir/MainActivity.java
+++ app/src/main/java/com/liweiyap/narradir/MainActivity.java
@@ -16,13 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);  // make the below show-/hide-changes temporary
+        // make the below show-/hide-changes temporary
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_IMMERSIVE |
+            // set the content to appear under the system bars
+            // so that the content does not resize when the system bars hide and show
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |  // hide navigation bar
-            View.SYSTEM_UI_FLAG_FULLSCREEN |  // hide status bar
-            View.SYSTEM_UI_FLAG_IMMERSIVE);
+            // hide navigation bar
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            // hide status bar
+            View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
