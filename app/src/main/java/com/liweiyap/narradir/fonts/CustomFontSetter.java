c4b32eaa50c52a37fcfccba3b67839c3dcceaaa5
liweiyap <liweiyap@gmail.com>
2021-03-07 05:32:13 +0800

    Rename all remaining fontName vars to assetFontPath

--- app/src/main/java/com/liweiyap/narradir/fonts/CustomFontSetter.java
+++ app/src/main/java/com/liweiyap/narradir/fonts/CustomFontSetter.java
@@ -11,16 +11,16 @@ import com.liweiyap.narradir.R;
 public class CustomFontSetter
 {
     /**
-     * Sets a font on a TextView based on the custom fontName attribute.
-     * If the custom fontName attribute isn't found in the attributes, nothing happens.
+     * Sets a font on a TextView based on the custom assetFontPath attribute.
+     * If the custom assetFontPath attribute isn't found in the attributes, nothing happens.
      * Can also be used on Buttons, because Button extends (is a subclass of) TextView.
      * Context required for getAssets() function.
      */
     public static void setCustomFont(TextView textView, Context context, AttributeSet attrs)
     {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomFont);
-        String fontName = typedArray.getString(R.styleable.CustomFont_fontName);
-        setCustomFont(textView, fontName, context);
+        String assetFontPath = typedArray.getString(R.styleable.CustomFont_assetFontPath);
+        setCustomFont(textView, assetFontPath, context);
         typedArray.recycle();
     }
 
