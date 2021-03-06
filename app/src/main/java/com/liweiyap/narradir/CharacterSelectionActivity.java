aecedea01f61b4ffd700b7f1bb4efa8bb33f97e2
liweiyap <liweiyap@gmail.com>
2021-03-07 07:39:56 +0800

    Add yellow LinearGradient to game title and fix fitsSystemWindows bug. Rename MainActivity to CharacterSelectionActivity

---
+++ app/src/main/java/com/liweiyap/narradir/CharacterSelectionActivity.java
@@ -0,0 +1,34 @@
+package com.liweiyap.narradir;
+
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CharacterSelectionActivity extends FullScreenPortraitActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setGameTitleTextViewColours();
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.testwavenet);
+        mp.start();
+//        mp.release();
+    }
+
+    private void setGameTitleTextViewColours()
+    {
+        TextView gameTitleTextView = findViewById(R.id.game_title);
+
+        int[] colourArray = new int[]{ getColor(R.color.ronchi), getColor(R.color.pirate_gold) };
+        Shader gameTitleTextViewShader = new LinearGradient(0, 0, 0, gameTitleTextView.getTextSize(), colourArray, null, Shader.TileMode.CLAMP);
+
+        gameTitleTextView.getPaint().setShader(gameTitleTextViewShader);
+        gameTitleTextView.setTextColor(colourArray[0]);
+    }
+}
--- app/src/main/java/com/liweiyap/narradir/fonts/CustomFontSetter.java
+++ app/src/main/java/com/liweiyap/narradir/fonts/CustomFontSetter.java
@@ -11,30 +11,30 @@ import com.liweiyap.narradir.R;
 public class CustomFontSetter
 {
     /**
-     * Sets a font on a TextView based on the custom fontName attribute.
-     * If the custom fontName attribute isn't found in the attributes, nothing happens.
+     * Sets a font on a TextView based on the custom assetFontPath attribute.
+     * If the custom assetFontPath attribute isn't found in the attributes, nothing happens.
      * Can also be used on Buttons, because Button extends (is a subclass of) TextView.
+     * Context required for getAssets() function.
      */
     public static void setCustomFont(TextView textView, Context context, AttributeSet attrs)
     {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomFont);
-        String fontName = typedArray.getString(R.styleable.CustomFont_fontName);
-        setCustomFont(textView, fontName, context);
+        String assetFontPath = typedArray.getString(R.styleable.CustomFont_assetFontPath);
+        setCustomFont(textView, assetFontPath, context);
         typedArray.recycle();
     }
 
     /**
      * Sets a font on a TextView
      */
-    public static void setCustomFont(TextView textView, String fontName, Context context)
+    public static void setCustomFont(TextView textView, String assetFontPath, Context context)
     {
-        if (fontName == null)
+        if (assetFontPath == null)
         {
             return;
         }
 
-        Typeface typeface = FontCache.get(fontName, context);
-
+        Typeface typeface = FontCache.get(assetFontPath, context);
         if (typeface != null)
         {
             textView.setTypeface(typeface);
