2026d0ffaa6f90192e2edeb4f19f1e6d2f6b4928
liweiyap <liweiyap@gmail.com>
2021-03-08 11:11:26 +0800

    Remove yellow LinearGradient and add text outline

--- app/src/main/java/com/liweiyap/narradir/CharacterSelectionActivity.java
+++ app/src/main/java/com/liweiyap/narradir/CharacterSelectionActivity.java
@@ -1,10 +1,7 @@
 package com.liweiyap.narradir;
 
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class CharacterSelectionActivity extends FullScreenPortraitActivity
 {
@@ -14,21 +11,8 @@ public class CharacterSelectionActivity extends FullScreenPortraitActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
 
-        setGameTitleTextViewColours();
-
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.testwavenet);
         mp.start();
 //        mp.release();
-    }
-
-    private void setGameTitleTextViewColours()
-    {
-        TextView gameTitleTextView = findViewById(R.id.game_title);
-
-        int[] colourArray = new int[]{ getColor(R.color.yuma), getColor(R.color.husk) };
-        Shader gameTitleTextViewShader = new LinearGradient(0, 0, 0, gameTitleTextView.getTextSize(), colourArray, null, Shader.TileMode.CLAMP);
-
-        gameTitleTextView.getPaint().setShader(gameTitleTextViewShader);
-        gameTitleTextView.setTextColor(colourArray[0]);
     }
 }
