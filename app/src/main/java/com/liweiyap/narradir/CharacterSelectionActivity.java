92119406c09cd355adcc25466cd8d0185ff64c8a
liweiyap <liweiyap@gmail.com>
2021-03-09 11:25:10 +0800

    Add background drawables for player number selection buttons in checked and unchecked states

--- app/src/main/java/com/liweiyap/narradir/CharacterSelectionActivity.java
+++ app/src/main/java/com/liweiyap/narradir/CharacterSelectionActivity.java
@@ -2,6 +2,15 @@ package com.liweiyap.narradir;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Pair;
+import android.widget.LinearLayout;
+
+import androidx.core.content.ContextCompat;
+
+import com.liweiyap.narradir.fontutil.CustomTypefaceableObserverButton;
+
+import java.util.Objects;
 
 public class CharacterSelectionActivity extends FullScreenPortraitActivity
 {
@@ -11,8 +20,59 @@ public class CharacterSelectionActivity extends FullScreenPortraitActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
 
+        setPlayerNumberSelectionButtonBackgrounds();
+        addSingleTargetSelectionToPlayerNumberSelectionLayout();
+
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.testwavenet);
         mp.start();
 //        mp.release();
     }
+
+    private void setPlayerNumberSelectionButtonBackgrounds()
+    {
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p5Button, new Pair<>(R.drawable.p5_button_checked_background, R.drawable.p5_button_unchecked_background));
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p6Button, new Pair<>(R.drawable.p6_button_checked_background, R.drawable.p6_button_unchecked_background));
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p7Button, new Pair<>(R.drawable.p7_button_checked_background, R.drawable.p7_button_unchecked_background));
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p8Button, new Pair<>(R.drawable.p8_button_checked_background, R.drawable.p8_button_unchecked_background));
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p9Button, new Pair<>(R.drawable.p9_button_checked_background, R.drawable.p9_button_unchecked_background));
+        playerNumberSelectionButtonBackgrounds.put(
+            R.id.p10Button, new Pair<>(R.drawable.p10_button_checked_background, R.drawable.p10_button_unchecked_background));
+    }
+
+    private void addSingleTargetSelectionToPlayerNumberSelectionLayout()
+    {
+        LinearLayout playerNumberSelectionLayout = findViewById(R.id.playerNumberSelectionLayout);
+        for (int childIdx = 0; childIdx < playerNumberSelectionLayout.getChildCount(); ++childIdx)
+        {
+            CustomTypefaceableObserverButton btn = (CustomTypefaceableObserverButton) playerNumberSelectionLayout.getChildAt(childIdx);
+            int i = childIdx;
+            btn.addOnClickObserver(() -> {
+                btn.setAlpha(1.f);
+                btn.setBackgroundDrawable(ContextCompat.getDrawable(this, Objects.requireNonNull(playerNumberSelectionButtonBackgrounds.get(btn.getId())).first));
+
+                for (int j = 0; j < playerNumberSelectionLayout.getChildCount(); ++j)
+                {
+                    if (i != j)
+                    {
+                        CustomTypefaceableObserverButton tmp = (CustomTypefaceableObserverButton) playerNumberSelectionLayout.getChildAt(j);
+                        tmp.setAlpha(0.5f);
+                        tmp.setBackgroundDrawable(ContextCompat.getDrawable(this, Objects.requireNonNull(playerNumberSelectionButtonBackgrounds.get(tmp.getId())).second));
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Key is an Integer representing the ID of the button.
+     * Value is a Pair of Integers.
+     *  - First Integer in Pair represents the ID of the background drawable of the button in the checked state.
+     *  - Second Integer in Pair represents the ID of the background drawable of the button in the unchecked state.
+     */
+    private final ArrayMap<Integer, Pair<Integer,Integer>> playerNumberSelectionButtonBackgrounds = new ArrayMap<>();
 }
