5ede4f7bb60a427360b8ec523fda2adab514b604
liweiyap <liweiyap@gmail.com>
2021-03-16 05:08:52 +0800

    Add audio files for introduction. Add simple example of passing data between Activites via Intents

--- app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
+++ app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
@@ -1,11 +1,17 @@
 package com.liweiyap.narradir;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import com.liweiyap.narradir.utils.FullScreenPortraitActivity;
 import com.liweiyap.narradir.utils.fonts.CustomTypefaceableObserverButton;
+
+import java.util.ArrayList;
 
 public class PlayIntroductionActivity extends FullScreenPortraitActivity
 {
@@ -14,6 +20,21 @@ public class PlayIntroductionActivity extends FullScreenPortraitActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_introduction);
+
+        Intent intent = getIntent();
+        ArrayList<Integer> introSegmentArrayList = intent.getIntegerArrayListExtra("INTRO_SEGMENTS");
+        if ( (introSegmentArrayList.size() != 4) &&
+             !((introSegmentArrayList.size() == 3) && (introSegmentArrayList.get(2) == R.raw.introsegment2nopercival)) )
+        {
+            throw new RuntimeException(
+                "PlayIntroductionActivity::onCreate(): " +
+                    "Invalid input from intent");
+        }
+
+        long pauseDurationInMilliSecs = intent.getLongExtra("PAUSE_DURATION", 5000);
+
+        MediaPlayer test = MediaPlayer.create(this, introSegmentArrayList.get(0));
+        test.start();
 
         CustomTypefaceableObserverButton stopButton = findViewById(R.id.playIntroLayoutStopButton);
         stopButton.addOnClickObserver(this::finish);
