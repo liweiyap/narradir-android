9b7a8755a2bbf5ef4042d9ba84f8ddfea541b639
liweiyap <liweiyap@gmail.com>
2021-03-16 12:52:40 +0800

    Play audio files one after another with pauses in between

--- app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
+++ app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
@@ -1,17 +1,17 @@
 package com.liweiyap.narradir;
 
-import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
-import android.util.Log;
 
 import com.liweiyap.narradir.utils.FullScreenPortraitActivity;
 import com.liweiyap.narradir.utils.fonts.CustomTypefaceableObserverButton;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class PlayIntroductionActivity extends FullScreenPortraitActivity
 {
@@ -31,12 +31,104 @@ public class PlayIntroductionActivity extends FullScreenPortraitActivity
                     "Invalid input from intent");
         }
 
-        long pauseDurationInMilliSecs = intent.getLongExtra("PAUSE_DURATION", 5000);
+        mPauseDurationInMilliSecs = intent.getLongExtra("PAUSE_DURATION", 5000);
 
-        MediaPlayer test = MediaPlayer.create(this, introSegmentArrayList.get(0));
-        test.start();
+        // https://stackoverflow.com/a/23856215/12367873
+        final Iterator<Integer> iter = introSegmentArrayList.iterator();
+        mIntroMediaPlayer = iter.hasNext() ?
+            MediaPlayer.create(this, iter.next()) :
+            null;
+        if (mIntroMediaPlayer != null)
+        {
+            mIntroMediaPlayer.start();
+            mIntroMediaPlayer.setOnCompletionListener(mediaPlayer -> {
+                // https://medium.com/androiddevelopers/deep-dive-mediaplayer-best-practices-feb4d15a66f5
+                mediaPlayer.reset();
+
+                if (!iter.hasNext())
+                {
+                    finish();
+                    return;
+                }
+
+                try
+                {
+                    // https://stackoverflow.com/a/20111291/12367873
+                    AssetFileDescriptor afd = PlayIntroductionActivity.this.getResources().openRawResourceFd(iter.next());
+                    if (afd == null)
+                    {
+                        return;
+                    }
+                    mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                    afd.close();
+                    mediaPlayer.prepare();
+
+                    mHandler.postDelayed(mediaPlayer::start, mPauseDurationInMilliSecs);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            });
+        }
 
         CustomTypefaceableObserverButton stopButton = findViewById(R.id.playIntroLayoutStopButton);
         stopButton.addOnClickObserver(this::finish);
     }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (mIntroMediaPlayer != null)
+        {
+            if (mWasPlaying)
+            {
+                mIntroMediaPlayer.seekTo(mIntroMediaPlayerCurrentLength);
+                mIntroMediaPlayer.start();
+            }
+            else
+            {
+                mHandler.postDelayed(mIntroMediaPlayer::start, mPauseDurationInMilliSecs);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (mIntroMediaPlayer != null)
+        {
+            if (mIntroMediaPlayer.isPlaying())
+            {
+                mIntroMediaPlayer.pause();
+                mIntroMediaPlayerCurrentLength = mIntroMediaPlayer.getCurrentPosition();
+                mWasPlaying = true;
+            }
+            else
+            {
+                mHandler.removeCallbacksAndMessages(null);
+                mWasPlaying = false;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (mIntroMediaPlayer != null)
+        {
+            mIntroMediaPlayer.release();
+            mIntroMediaPlayer = null;
+        }
+    }
+
+    private MediaPlayer mIntroMediaPlayer;
+    private int mIntroMediaPlayerCurrentLength;
+    private final Handler mHandler = new Handler();
+    private long mPauseDurationInMilliSecs = 5000;
+    private boolean mWasPlaying = false;
 }
