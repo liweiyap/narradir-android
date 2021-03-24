f0c0eedae1f446347d300453cb0bee8046373e86
liweiyap <liweiyap@gmail.com>
2021-03-24 16:36:11 +0800

    Add ObserverButton class

--- app/src/main/java/com/liweiyap/narradir/utils/fonts/CustomTypefaceableObserverButton.java
+++ app/src/main/java/com/liweiyap/narradir/utils/fonts/CustomTypefaceableObserverButton.java
@@ -2,50 +2,37 @@ package com.liweiyap.narradir.utils.fonts;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.liweiyap.narradir.utils.Observer;
-import com.liweiyap.narradir.utils.ObserverListener;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.liweiyap.narradir.utils.ObserverButton;
 
 public class CustomTypefaceableObserverButton
-    extends androidx.appcompat.widget.AppCompatButton
-    implements CustomTypefaceable, View.OnClickListener, View.OnLongClickListener, ObserverListener
+    extends ObserverButton
+    implements CustomTypefaceable
 {
     public CustomTypefaceableObserverButton(@NonNull Context context)
     {
         super(context);
-        setOnClickListener(this);
-        setOnLongClickListener(this);
     }
 
     public CustomTypefaceableObserverButton(@NonNull Context context, @Nullable AttributeSet attrs)
     {
         super(context, attrs);
         setCustomTypeface(context, attrs);
-        setOnClickListener(this);
-        setOnLongClickListener(this);
     }
 
     public CustomTypefaceableObserverButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
         setCustomTypeface(context, attrs);
-        setOnClickListener(this);
-        setOnLongClickListener(this);
     }
 
     public CustomTypefaceableObserverButton(@NonNull Context context, final String assetFontPath)
     {
         this(context);
         setCustomTypeface(context, assetFontPath);
-        setOnClickListener(this);
-        setOnLongClickListener(this);
     }
 
     @Override
@@ -59,44 +46,4 @@ public class CustomTypefaceableObserverButton
     {
         CustomFontSetter.setCustomFont(this, assetFontPath, context);
     }
-
-    @Override
-    public void onClick(View view)
-    {
-        notifyOnClickObservers();
-    }
-
-    @Override
-    public boolean onLongClick(View view)
-    {
-        notifyOnLongClickObservers();
-        return true;  // https://stackoverflow.com/a/3756619/12367873; https://stackoverflow.com/questions/4402740/android-long-click-on-a-button-perform-actions
-    }
-
-    @Override
-    public void addOnClickObserver(Observer observer)
-    {
-        mOnClickObservers.add(observer);
-    }
-
-    @Override
-    public void notifyOnClickObservers()
-    {
-        mOnClickObservers.forEach(Observer::update);
-    }
-
-    @Override
-    public void addOnLongClickObserver(Observer observer)
-    {
-        mOnLongClickObservers.add(observer);
-    }
-
-    @Override
-    public void notifyOnLongClickObservers()
-    {
-        mOnLongClickObservers.forEach(Observer::update);
-    }
-
-    protected final List<Observer> mOnClickObservers = new ArrayList<>();
-    protected final List<Observer> mOnLongClickObservers = new ArrayList<>();
 }
