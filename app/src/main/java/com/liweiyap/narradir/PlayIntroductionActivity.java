eab287b1c5593b433cd6cd6abee3f0d234b9c1f3
liweiyap <liweiyap@gmail.com>
2021-03-31 19:42:09 +0800

    Add keystore for release mode. Update gradle plugin

--- app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
+++ app/src/main/java/com/liweiyap/narradir/PlayIntroductionActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.SoundPool;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
