dad79e6a38392708b30eb266d1624d2955924653
liweiyap <liweiyap@gmail.com>
2021-05-08 16:27:23 +0100

    Refactor SettingsBackgroundActivity

--- app/src/main/java/com/liweiyap/narradir/SettingsBackgroundActivity.java
+++ app/src/main/java/com/liweiyap/narradir/SettingsBackgroundActivity.java
@@ -13,6 +13,7 @@ import androidx.annotation.RawRes;
 import com.liweiyap.narradir.utils.ActiveFullScreenPortraitActivity;
 import com.liweiyap.narradir.utils.ObserverButton;
 import com.liweiyap.narradir.utils.ObserverListener;
+import com.liweiyap.narradir.utils.ViewGroupSingleTargetSelector;
 import com.liweiyap.narradir.utils.fonts.CustomTypefaceableCheckableObserverButton;
 import com.liweiyap.narradir.utils.fonts.CustomTypefaceableObserverButton;
 import com.liweiyap.narradir.utils.fonts.CustomTypefaceableTextView;
@@ -139,22 +140,13 @@ public class SettingsBackgroundActivity extends ActiveFullScreenPortraitActivity
 
     private void addSingleTargetSelectionToBackgroundSoundSelectionLayout()
     {
-        LinearLayout backgroundSoundSelectionLayout = findViewById(R.id.backgroundSoundSelectionLayout);
-        for (int childIdx = 0; childIdx < backgroundSoundSelectionLayout.getChildCount(); ++childIdx)
+        try
         {
-            CustomTypefaceableCheckableObserverButton btn = (CustomTypefaceableCheckableObserverButton) backgroundSoundSelectionLayout.getChildAt(childIdx);
-            int i = childIdx;
-            btn.addOnClickObserver(() -> {
-                btn.check();
-                for (int j = 0; j < backgroundSoundSelectionLayout.getChildCount(); ++j)
-                {
-                    if (i != j)
-                    {
-                        CustomTypefaceableCheckableObserverButton tmp = (CustomTypefaceableCheckableObserverButton) backgroundSoundSelectionLayout.getChildAt(j);
-                        tmp.uncheck();
-                    }
-                }
-            });
+            ViewGroupSingleTargetSelector.addSingleTargetSelection(findViewById(R.id.backgroundSoundSelectionLayout));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 
