1a9da2f0955cfd106c3be023b44e708aea10cf57
Li-Wei Yap <liweiyap@gmail.com>
2022-12-29 22:02:26 +0100

    [Kotlin] Migrate StrokedCustomTypefaceableTextView from Java to Kotlin

--- app/src/main/java/com/liweiyap/narradir/ui/fonts/IOutlineable.kt
+++ app/src/main/java/com/liweiyap/narradir/ui/fonts/IOutlineable.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import androidx.annotation.ColorInt
 
 interface IOutlineable {
-    fun setStroke(context: Context?, attrs: AttributeSet?)
+    fun setStroke(context: Context, attrs: AttributeSet?)
     fun setStrokeColor(@ColorInt color: Int)
     fun setStrokeWidth(width: Float)
     fun setStrokeWidth(unit: Int, width: Float)
