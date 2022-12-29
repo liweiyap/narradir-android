f2691f4a4e1bb92c2a40479586569d040efd57a0
Li-Wei Yap <liweiyap@gmail.com>
2022-12-29 14:45:11 +0100

    [Kotlin] Migrate CustomTypefaceableTextView from Java to Kotlin

---
+++ app/src/main/java/com/liweiyap/narradir/ui/fonts/CustomTypefaceableTextView.kt
@@ -0,0 +1,37 @@
+package com.liweiyap.narradir.ui.fonts
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.appcompat.widget.AppCompatTextView
+
+import com.liweiyap.narradir.util.fonts.CustomFontSetter
+
+/**
+ * https://stackoverflow.com/questions/16901930/memory-leaks-with-custom-font-for-set-custom-font/16902532#16902532
+ */
+open class CustomTypefaceableTextView:
+    AppCompatTextView,
+    ICustomTypefaceable {
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs) {
+        setCustomTypeface(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
+        setCustomTypeface(context, attrs)
+    }
+
+    constructor(context: Context, assetFontPath: String?): this(context) {
+        setCustomTypeface(context, assetFontPath)
+    }
+
+    final override fun setCustomTypeface(context: Context, attrs: AttributeSet?) {
+        CustomFontSetter.setCustomFont(this, context, attrs)
+    }
+
+    final override fun setCustomTypeface(context: Context, assetFontPath: String?) {
+        CustomFontSetter.setCustomFont(this, assetFontPath, context)
+    }
+}
--- app/src/main/java/com/liweiyap/narradir/ui/fonts/ICustomTypefaceable.kt
+++ app/src/main/java/com/liweiyap/narradir/ui/fonts/ICustomTypefaceable.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 
 interface ICustomTypefaceable {
-    fun setCustomTypeface(context: Context?, attrs: AttributeSet?)
-    fun setCustomTypeface(context: Context?, assetFontPath: String?)
+    fun setCustomTypeface(context: Context, attrs: AttributeSet?)
+    fun setCustomTypeface(context: Context, assetFontPath: String?)
 }
