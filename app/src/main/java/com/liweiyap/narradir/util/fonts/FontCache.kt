be82369f6fb4d1ec6c61de4642e593bc25e59ece
Li-Wei Yap <liweiyap@gmail.com>
2022-12-29 13:18:07 +0100

    [Kotlin] Migrate FontCache from Java to Kotlin

---
+++ app/src/main/java/com/liweiyap/narradir/util/fonts/FontCache.kt
@@ -0,0 +1,38 @@
+package com.liweiyap.narradir.util.fonts
+
+import android.content.Context
+import android.graphics.Typeface
+import androidx.collection.ArrayMap
+import java.lang.Exception
+
+/**
+ * Caches the custom Typefaces in the assets directory to:
+ * - avoid memory leaks on older handsets (https://issuetracker.google.com/issues/36919609)
+ * - increase execution speed at run time, since it's not super fast to read from assets all the time.
+ */
+object FontCache {
+    private val sFontCache = ArrayMap<String, Typeface>()
+
+    @JvmStatic
+    fun get(assetFontPath: String?, context: Context): Typeface? {
+        if (assetFontPath == null) {
+            return null
+        }
+
+        synchronized(sFontCache) {
+            var typeface = sFontCache[assetFontPath]
+
+            if (typeface == null) {
+                try {
+                    typeface = Typeface.createFromAsset(context.assets, assetFontPath)
+                } catch (e: Exception) {
+                    return null
+                }
+
+                sFontCache[assetFontPath] = typeface
+            }
+
+            return typeface
+        }
+    }
+}
