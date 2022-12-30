a860d455293d64925186968f0ea2f16e86c77f31
Li-Wei Yap <liweiyap@gmail.com>
2022-12-30 13:13:01 +0100

    [Kotlin] Migrate TimeDisplay from Java to Kotlin

--- app/src/main/java/com/liweiyap/narradir/util/TimeDisplay.kt
+++ app/src/main/java/com/liweiyap/narradir/util/TimeDisplay.kt
@@ -1,16 +1,11 @@
-package com.liweiyap.narradir.util;
+package com.liweiyap.narradir.util
 
-import androidx.annotation.NonNull;
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
-public final class TimeDisplay
-{
-    private TimeDisplay(){}
-
-    public static @NonNull String fromMilliseconds(final long msec)
-    {
+object TimeDisplay {
+    @JvmStatic
+    fun fromMilliseconds(msec: Long): String {
         return String.format(
             // guaranteed to be available on all devices, has no surprising special cases, and tends to be most efficient due to its frequency of use
             // (https://developer.android.com/reference/java/util/Locale.html#default_locale)
@@ -18,6 +13,6 @@ public final class TimeDisplay
             "%02d:%02d",
             TimeUnit.MILLISECONDS.toMinutes(msec) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(msec)),
             TimeUnit.MILLISECONDS.toSeconds(msec) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(msec))
-        );
+        )
     }
 }
