62341cde44d7e7ae9ba46c784e2fb3efe2bde709
Li-Wei Yap <liweiyap@gmail.com>
2022-12-30 12:43:38 +0100

    [Kotlin] Migrate ViewGroupSingleTargetSelector from Java to Kotlin

---
+++ app/src/main/java/com/liweiyap/narradir/ui/ViewGroupSingleTargetSelector.kt
@@ -0,0 +1,47 @@
+package com.liweiyap.narradir.ui
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Checkable
+
+import com.liweiyap.narradir.util.IObserver
+
+object ViewGroupSingleTargetSelector {
+    @Throws(RuntimeException::class)
+    @JvmStatic
+    fun addSingleTargetSelection(viewGroup: ViewGroup?) {
+        if (viewGroup == null) {
+            return
+        }
+
+        for (childIdx: Int in 0 until viewGroup.childCount) {
+            val view: View = viewGroup.getChildAt(childIdx)
+
+            if (view !is Checkable) {
+                throw RuntimeException(
+                    "ViewGroupSingleTargetSelector::addSingleTargetSelection(): " +
+                            "Programming error: View is not Checkable."
+                )
+            }
+
+            if (view !is IObserverListener) {
+                throw RuntimeException(
+                    "ViewGroupSingleTargetSelector::addSingleTargetSelection(): " +
+                            "Programming error: View is not ObserverListener."
+                )
+            }
+
+            (view as IObserverListener).addOnClickObserver(object: IObserver {
+                override fun update() {
+                    (view as Checkable).isChecked = true
+                    for (idx in 0 until viewGroup.childCount) {
+                        if (childIdx != idx) {
+                            val tmp = viewGroup.getChildAt(idx)
+                            (tmp as Checkable).isChecked = false
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
