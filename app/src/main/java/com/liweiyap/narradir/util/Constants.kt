17d799eb5faadc4e6e4dc2daa25b4c945c0cae3c
Li-Wei Yap <liweiyap@gmail.com>
2022-12-30 13:29:59 +0100

    [Kotlin] Migrate Constants from Java to Kotlin

---
+++ app/src/main/java/com/liweiyap/narradir/util/Constants.kt
@@ -0,0 +1,17 @@
+package com.liweiyap.narradir.util
+
+object Constants {
+    const val GAME_AVALON: Int = 0
+    const val GAME_SECRETHITLER: Int = 1
+
+    const val REQUEST_SETTINGS_HOME: Int = 100
+    const val RESULT_OK_SETTINGS_HOME: Int = 101
+
+    const val REQUEST_SETTINGS_NEW: Int = 200
+    const val RESULT_OK_SETTINGS_ONESTEP: Int = 201
+    const val RESULT_OK_SETTINGS_TWOSTEPS: Int = 202
+
+    const val GUIDANCE_NULL: Int = 300
+    const val GUIDANCE_HELP: Int = 301
+    const val GUIDANCE_PRIVACY: Int = 302
+}
